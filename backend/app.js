b1dfdb78ea7e0efcdfcc43c203472b490e78a239
Kavithan13 <nagamkavitha1999@gmail.com>
2026-06-12 05:25:31 +0000

    Updated backend image to v2

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Backend 🚀" });
+app.get("/", (req, res) => {
+  res.send("Backend is running");
 });
 
 app.listen(3000, () => {
